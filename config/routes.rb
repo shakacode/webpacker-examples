38e2289d92ac6e3e2eff54f3f31b8e65802b864d
Justin Gordon <justin.gordon@gmail.com>
2020-02-02 21:19:56 -1000

    No Code Splitting
    2 Routes
    /sort which has Lodash + Moment
    /sort-backwards which has only Lodash
    
    Downloads for both bundles (packs) duplicate the lodash code.
    
    This results in large downloads for both pages.

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#index'
   get 'pages', to: 'pages#index'
-  get 'simple_html', to: 'pages#simple_html'
+  get 'simple-html', to: 'pages#simple_html'
+  get 'sort', to: 'sort#index'
+  get 'sort-backwards', to: 'sort_backwards#index'
 end
