ac7d5931cbb973aff1eb582eddfa1605cc6fed2f
Justin Gordon <justin.gordon@gmail.com>
2020-02-02 14:48:52 -1000

    Rails server on production serves assets from puma
    Without this change, assets in public/webpack are not given to the browser.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -22,7 +22,10 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  # Need to make sure this is true when using puma
+  config.public_file_server.enabled = true
+  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
