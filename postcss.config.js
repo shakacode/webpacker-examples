aaf0f92fd01314e8d6936495763c046137ec43ce
Justin Gordon <justin.gordon@gmail.com>
2020-02-02 15:29:21 -1000

    Install prettier plus eslint with airbnb config
    This command will pretty and lint with fixes:
    
    yarn run lint-fix

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,13 @@
+/* eslint-disable import/no-extraneous-dependencies, global-require */
 module.exports = {
   plugins: [
-    require('postcss-import'),
-    require('postcss-flexbugs-fixes'),
-    require('postcss-preset-env')({
+    require("postcss-import"),
+    require("postcss-flexbugs-fixes"),
+    require("postcss-preset-env")({
       autoprefixer: {
-        flexbox: 'no-2009'
+        flexbox: "no-2009"
       },
       stage: 3
     })
   ]
-}
+};
