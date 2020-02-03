4f8d4caa4575de352811ab67472bcc072dff24f6
Justin Gordon <justin.gordon@gmail.com>
2020-02-02 14:46:10 -1000

    Add Rubocop defaults

--- app/controllers/hello_world_controller.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class HelloWorldController < ApplicationController
-
-  def index
-  end
-end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -14,5 +14,3 @@
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
-
-
