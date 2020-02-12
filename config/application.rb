7e16f6d1a511e0f4fa45adcd3b84654f92b1e209
Justin Gordon <justin.gordon@gmail.com>
2020-02-11 20:26:01 -1000

    Rename app from Bare1 to webpacker-examples

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,7 @@ require 'action_cable/engine'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Bare1
+module WebpackerExamples
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
