c919f7d167e03859cef21c07ddfe5507c8e12bf8
Justin Gordon <justin.gordon@gmail.com>
2020-02-12 16:18:35 -1000

    Add title and description meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+
+  def title(sub_title)
+    content_for(:title) { sub_title }
+  end
+
+  def description(description)
+    content_for(:meta_description) { description }
+  end
 end
