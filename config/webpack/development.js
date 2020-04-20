3611f13e1feab1d7b2e555dc13b4ee159deb1802
Justin Gordon <justin.gordon@gmail.com>
2020-04-19 15:35:07 -1000

    Update to rails/webpacker 5.1

--- config/webpack/development.js
+++ config/webpack/development.js
@@ -5,4 +5,12 @@ process.env.NODE_ENV = process.env.NODE_ENV || "development";
 
 environment.plugins.prepend("DashboardPlugin", new DashboardPlugin());
 
-module.exports = environment.toWebpackConfig();
+const webpackConfiguration = environment.toWebpackConfig();
+
+// To inspect the webpackConfiguration, uncomment the next line
+// debugger;
+
+// Then run:
+// bin/webpack --debug
+
+module.exports = webpackConfiguration;
