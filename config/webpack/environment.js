96fddb91381bd6b37114dff26dcd067d16fbeabc
Justin Gordon <justin.gordon@gmail.com>
2020-04-21 10:48:01 -1000

    Add webpack-bundle-analyzer

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,9 @@
 const { environment } = require("@rails/webpacker");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+environment.plugins.prepend("BundleAnalyzerPlugin", new BundleAnalyzerPlugin());
 
 // Enable the default config of split chunks
 environment.splitChunks();
-
-// or using custom config
-// const nodeEnv = process.env.NODE_ENV;
-// environment.splitChunks(config => ({
-//   ...config,
-//   optimization: {
-//     splitChunks: {
-//       chunks: "all",
-//       // Prefer this in production
-//       // https://webpack.js.org/plugins/split-chunks-plugin/#splitchunksname
-//       // name: nodeEnv !== 'production'
-//       // Use this for the demo to show the descriptive chunk names
-//       name: true
-//     },
-//     runtimeChunk: true
-//   }
-// }));
 
 module.exports = environment;
